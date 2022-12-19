391153bed3fcad23d46e06710326743a8694ae87
RajKKapadia <raajforyou@gmail.com>
2022-12-19 16:17:51 +0530

    blog list using props success

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,12 +1,37 @@
-// import { useState } from 'react';
+import { useState } from 'react';
+import BlogList from './BlogList';
 
 const Home = () => {
 
-    // const [blogs, setBlogs] = useState();
+    const [blogs, setBlogs] = useState([
+        {
+            title: 'Test 1',
+            body: 'Test body for the blog.',
+            author: 'Test',
+            id: 1
+        },
+        {
+            title: 'Test 2',
+            body: 'Test body for the blog.',
+            author: 'Test',
+            id: 2
+        },
+        {
+            title: 'Test 3',
+            body: 'Test body for the blog.',
+            author: 'Test',
+            id: 3
+        }
+    ]);
+
+    const handleDelete = (id) => {
+        let newBlogs = blogs.filter((blog) => blog.id !== id);
+        setBlogs(newBlogs);
+    };
 
     return (
         <div className="home">
-            <h1>Home page</h1>
+            <BlogList blogs={blogs} title={'Raj'} handleDelete={handleDelete} />
         </div>
     );
 };
